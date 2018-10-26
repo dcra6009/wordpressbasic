25e2a38c1636129bbc729e5a6a15c1eef8e855a8
Hoàng Bùi <hoang.bui@galaxydr.com.vn>
2018-10-26 17:04:54 +0700

    update views

--- themes/01_basictheme/404.php
+++ themes/01_basictheme/404.php
@@ -3,10 +3,12 @@
 <div class="row">
 	<div class="col-8">
 	<main id="content" class="content ml-4">
-		<div class="entry-box clearfix">
-				<p><?php _e( 'Page not found!', 'basic' ); ?></p>
-				<?php get_search_form(); ?>
-		</div>
+			<article class="post page m-5" id="pageid-<?php the_ID(); ?>">
+				<h1><?php _e( 'Page not found!', 'basic' ); ?></h1>
+				<p class="border-top">
+					<?php get_search_form(); ?>
+				</p>
+			</article>
 	</main>
 	<!-- END #content -->
 	<div class="ml-3 border-bottom">
--- themes/01_basictheme/comments.php
+++ themes/01_basictheme/comments.php
@@ -1,6 +1,6 @@
-<div id="comments" class="comments-area">
+<div id="comments" class="comments-area border-top">
 <?php if ( have_comments() ) : ?>
-	<h3 class="comments-title"><?php _e('Comments', 'basic'); ?>
+	<h3 class="comments-title mt-2"><?php _e('Comments', 'basic'); ?>
 		<span class="cnt"><i class="fa fa-comments-o"></i><?php comments_number('0', '1', '%' );?></span></h3>
 			<ul class="comment-list">
 				<?php
@@ -15,7 +15,6 @@
 
 <?php endif; // have_comments()
 
-	comment_form();
 ?>
 <div class="ml-3 border-bottom">
 	<small><em>comments.php</em></small>
